c12c4986b88c47fdbc78fc3e9321a291e7552e6d
nitadragosandrei <nitadragosandrei@gmail.com>
2022-06-21 20:13:40 +0300

    Changed readme file, added a background page and changed the icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+       'paint-brushes': "url('../public/background.jpg')",
+      })
+    }
   },
   plugins: [],
 }
